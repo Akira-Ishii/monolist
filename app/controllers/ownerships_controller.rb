3bb7a512195934d85075caeeac8789ae878b5d70
Akira Ishii <akira-ishii@united.jp>
2016-04-16 03:19:34 +0000

    partialy success

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -12,7 +12,10 @@ class OwnershipsController < ApplicationController
     if @item.new_record?
       begin
         # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
-        response = Amazon::Ecs.item_lookup(params[:asin])
+        response = Amazon::Ecs.item_lookup(params[:asin] , 
+                                  :search_index => 'All' , 
+                                  :response_group => 'Medium' , 
+                                  :country => 'jp')
       rescue Amazon::RequestError => e
         return render :js => "alert('#{e.message}')"
       end
@@ -31,9 +34,9 @@ class OwnershipsController < ApplicationController
     # params[:type]の値にHaveボタンが押された時には「Have」,
     # Wantボタンが押された時には「Want」が設定されています。
     
-    if params[:type].eql?('Have')
+    if params[:type] == ('Have')
       current_user.have(@item)
-    elsif params[:type].eql?('Want')
+    elsif params[:type] == ('Want')
       current_user.want(@item)
     end
       
@@ -48,9 +51,9 @@ class OwnershipsController < ApplicationController
     # params[:type]の値にHave itボタンが押された時には「Have」,
     # Want itボタンが押された時には「Want」が設定されています。
     
-    if params[:type].eql?('Have')
+    if params[:type] == ('Have')
       current_user.unhave(@item)
-    elsif params[:type].eql?('Want')
+    elsif params[:type] == ('Want')
       current_user.unwant(@item)
     end    
 
